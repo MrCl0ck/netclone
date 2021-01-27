ad6605e67d910db5078a55a134e98efd2b73e529
Levi Pedreira <levipedreira27@gmail.com>
2021-01-27 17:06:16 -0300

    Adição de funções JS que limpasse a tela de trailer e renderizassem o trailer de cada filme apresentado na tela ao passar o mouse por cima da imagem deles (carousel), e do botão assistir agora na classe '.filme-principal'.

--- js/main.js
+++ js/main.js
@@ -14,3 +14,27 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+function video_in(classe, filme){
+    video_out();
+    let container = document.querySelector(classe);
+    let divVideo = document.createElement("video");
+    divVideo.setAttribute("autoplay","");
+    divVideo.setAttribute("width","720");divVideo.setAttribute("loop","");
+    divVideo.setAttribute("preload","metadata");
+    let sourceElement = document.createElement("source");
+    sourceElement.setAttribute("src","video/"+filme+".mp4");sourceElement.setAttribute("type","video/mp4");
+    divVideo.insertAdjacentElement("afterbegin",sourceElement);
+    divVideo.classList.add("video");
+    container.insertAdjacentElement("beforeend",divVideo);
+    divVideo.classList.toggle("show");    
+}
+
+function video_out(){
+    let container = document.querySelector('div .video');
+    if (container !== null) {
+        container.remove();        
+    }
+    /* container[0].innerHTML = "";
+    container[0].style.visibility = "invisible"; */
+}
